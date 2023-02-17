694decf5c205e4bed6e607332ec0ede0ecc5ff42
PK <nozumaz@gmail.com>
2023-02-17 13:29:34 -0500

    loops through cards and counts down

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+
+    const msgs = [];
+
+    for (let i = 0; i < names.length; i++) {
+        msgs.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+
+    return msgs;
+}
+
+function countDown(n) {
+
+    while (n >= 0) {
+        console.log(n--)
+    }
+
+}
